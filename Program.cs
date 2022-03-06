f1684746db1755b7d5196eb142c68b75985bb449
mandyee <minju4144@gmail.com>
2022-03-06 16:49:48 +0900

    #3 Remove unnecessary variable and case

--- Program.cs
+++ Program.cs
@@ -9,18 +9,9 @@ namespace _20220304_Cinema
             Seat seat = new Seat();
             Customer customer = new Customer();
 
-            Boolean auto = true;
-
-            while (auto)
+            while (true)
             {
-                Console.WriteLine("**************************");
-                Console.WriteLine("*****영화 예매 시스템*****");
-                Console.WriteLine("**************************");
-                Console.Write("1. 예매하기\n\n2. 예매조회\n\n3. 예매취소\n\n");
-
-                int input = int.Parse(Console.ReadLine());
-
-                switch (input)
+                switch (seat.DisplayMenu())
                 {
                     case 1:
                         seat.PrintSeat();
@@ -34,11 +25,7 @@ namespace _20220304_Cinema
                         break;
                     case 4:
                         Console.WriteLine("종료되었습니다.");
-                        auto = false;
-                        continue;
-                    default:
-                        Console.WriteLine("올바른 메뉴를 선택하세요\n");
-                        continue;
+                        break;
                 }
             }
         }
