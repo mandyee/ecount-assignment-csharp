a5eed4977fa68ed64c6adab0fad5cd4586b60558
mandyee <minju4144@gmail.com>
2022-03-06 19:44:33 +0900

    #7 Move DisplayMenu method to Cinema class

--- Cinema.cs
+++ Cinema.cs
@@ -17,11 +17,38 @@ namespace _20220304_Cinema
             customer = new Customer(row, col);
         }
 
+        public int DisplayMenu()
+        {
+            int input;
+
+            do
+            {
+                try
+                {
+                    Console.WriteLine("**************************");
+                    Console.WriteLine("*****영화 예매 시스템*****");
+                    Console.WriteLine("**************************");
+                    Console.Write("1. 예매하기\n\n2. 예매조회\n\n3. 예매취소\n\n4. 종료\n\n");
+
+                    input = int.Parse(Console.ReadLine());
+
+                    if (input >= 1 && input <= 4) break;
+                    else throw new Exception("올바른 메뉴를 선택하세요.\n");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            } while (true);
+
+            return input;
+        }
+
         public void Start()
         {
             while (true)
             {
-                switch (seat.DisplayMenu())
+                switch (DisplayMenu())
                 {
                     case 1:
                         seat.PrintSeat();
--- Seat.cs
+++ Seat.cs
@@ -84,32 +84,5 @@ namespace _20220304_Cinema
                 }
             }
         }
-
-        public int DisplayMenu()
-        {
-            int input;
-
-            do
-            {
-                try
-                {
-                    Console.WriteLine("**************************");
-                    Console.WriteLine("*****영화 예매 시스템*****");
-                    Console.WriteLine("**************************");
-                    Console.Write("1. 예매하기\n\n2. 예매조회\n\n3. 예매취소\n\n4. 종료\n\n");
-
-                    input = int.Parse(Console.ReadLine());
-
-                    if (input >= 1 && input <= 4) break;
-                    else throw new Exception("올바른 메뉴를 선택하세요.\n");
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-            } while (true);
-
-            return input;
-        }
     }
 }
